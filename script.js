108682b0f73e76fd56b847d71c2b95044bed88b4
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-05-26 13:40:07 -0700

    updated localstorage function, checked in console

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ $(function () {
   // useful when saving the description in local storage?
   $('.saveBtn').click(function(event){
     event.preventDefault();
-    var scheduleItem = $(this).siblings('.description').val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(scheduleItem, time);
+    var savedInput = JSON.parse(localStorage.getItem('input')) || []
+    var input = {
+    scheduleItem: $(this).siblings('.description').val(),
+    time: $(this).parent().attr("id"),
+    }
+    savedInput.push(input);
+    localStorage.setItem('input', JSON.stringify(savedInput));
   })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -100,6 +104,7 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  
   
 
   // TODO: Add code to display the current date in the header of the page.
