69d22562e816ef2f82bfdd1b2f67d224ba96384b
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-06-01 10:57:53 -0700

    clarified/edited comments

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-// ADDITIONAL: button at top of page to clear all events for that day and local storage if the user wishes to
-$('.btn-secondary').click(function(){
-  $('.description').text("");
-  localStorage.clear();
-});
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Wrapping all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // event listener for clicks on the save button using jquery. uses 'this' keyword to traverse the DOM based on the save button's location that the user clicks on ('this' is referring to the button, and then traverses the DOM by going to its sibling elements and finding the 'description' class (the value in localStorage's parameters), and to its parent element to find the hour id (the key in localStorage's parameters)).
   $('.saveBtn').click(function(event){
     event.preventDefault();
     var scheduleItem = $(this).siblings('.description').val();
@@ -20,11 +8,9 @@ $(function () {
 
     localStorage.setItem(hour, scheduleItem);
   });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+  // code applies the past, present, or future class to each time block by comparing the id hour to the current hour. 
+  // Dayjs is used to get the current hour, as well as declare the hour of day for each time block. Code uses Dayjs syntax to compare the current hour to that time block's hour (isSame, isAfter)
   var currentHour = dayjs().hour();
   
   var timeBlock09 = dayjs(09);
@@ -101,9 +87,7 @@ $(function () {
   }
   
   
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // code gets any user input that was saved in localStorage and sets the values of the corresponding textarea elements
   $('#hour-09 .description').text(localStorage.getItem('hour-09'));
   $('#hour-10 .description').text(localStorage.getItem('hour-10'));
   $('#hour-11 .description').text(localStorage.getItem('hour-11'));
@@ -115,7 +99,7 @@ $(function () {
   $('#hour-17 .description').text(localStorage.getItem('hour-17'));
   
 
-  // TODO: Add code to display the current date in the header of the page.
+  // displays the current date at the top of the page
   var currentDay = dayjs().format('dddd, MMM D, YYYY');
 $('#currentDay').text("Today is " + currentDay);
 
@@ -125,6 +109,11 @@ $('.close').click(function(){
   var hour = $(this).parent().attr("id");
   scheduleItem.text("");
   localStorage.removeItem(hour, scheduleItem);
-})
+});
+// ADDITIONAL: button at top of page to clear all events for that day and local storage if the user wishes to
+$('.btn-secondary').click(function(){
+  $('.description').text("");
+  localStorage.clear();
+});
 });
 
